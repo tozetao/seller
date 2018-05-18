e56cb55b67943266a9fdac8dc1371e4db9561e76
tozetao <tozetao@163.com>
2018-05-18 18:54:09 +0800

    5-18

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper"></div>
+        <div class="menu-wrapper">
+            <ul>
+                <li class="menu-item" v-for="(item, index) in goods" :key="index">
+                    <span class="icon" v-show="item.type>0" :class="iconMap[item.type]"></span>
+                </li>
+            </ul>
+        </div>
         <div class="foods-wrapper"></div>
     </div>
 </template>
@@ -27,6 +33,7 @@ export default {
                     console.log(this.goods)
                 }
             })
+            this.iconMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
         })
     }
 }
@@ -43,6 +50,27 @@ export default {
         flex 0 0 80px
         width: 80px
         background #f3f5f7
+        .menu-item
+            display: table
+            height:54px
+            width:56px
+            line-height:14px
+            .icon
+                vertical-align top
+                display inline-block
+                width 12px
+                height: 12px
+                background-repeat no-repeat
+                background-size 12px 12px
+                margin-right 4px
+                &.decrease
+                    bg-image('decrease_1')
+                &.discount
+                    bg-image('discount_1')
+                &.invoice
+                    bg-image('invoice_1')
+                &.guarantee
+                    bg-image('guarantee_1')
     .foods-wrapper
         flex: 1
 </style>
